ad4316c07a4ed33b1f13b82c2eaf6ee6725d8f51
crosbya1 <crosby@ualberta.ca>
2026-04-08 08:52:15 -0600

    April 8

--- 1_scripts/00.1_predict_practice_code.R
+++ 1_scripts/00.1_predict_practice_code.R
@@ -4,45 +4,33 @@ library(sf)
 library(tidyverse)
 library(ggplot2)
 
-d <- st_read("0_data/processed/g2_int_test.gdb", "g2_int_test")
-g <- st_read("0_data/processed/g2_grid_test.shp")
+d <- read.csv("0_data/processed/osr_grid_intersection_bfveg_2022.csv")
+g <- st_read("0_data/processed/shapefiles/osr_grid.shp"); gc()
 
-save(d, g, file = "0_data/processed/g2_test_data.Rdata")
 
-a <- as.numeric(st_area(d))
+# all.equal(d$RECALCAREA, d$Shape_Area)
 
-all.equal(a, d$Shape_Area)
-
-d$RECALCAREA <- as.numeric(st_area(d))
-
-AREA_COL<-"RECALCAREA"
-if (AREA_COL != "Shape_Area") {
-  d[["Shape_Area"]] <- d[[AREA_COL]]
-  d[[AREA_COL]] <- NULL
-}
-cat("OK\n\n")
-
-lc <- d
+## If above result != 'FALSE'
+# AREA_COL<-"RECALCAREA"
+# if (AREA_COL != "Shape_Area") {
+#   d[["Shape_Area"]] <- d[[AREA_COL]]
+#   d[[AREA_COL]] <- NULL
+# }
+# cat("OK\n\n")
 
 # Correct the feature types based on the 2022 HFI classes
-lc$FEATURE_TY <- as.character(lc$FEATURE_TY)
-lc$FEATURE_TY[lc$FEATURE_TY %in% c("TIMBER-HARVEST-GREEN-AREA", "TIMBER-HARVEST-WHITE-AREA")] <- "HARVEST-AREA"
-lc$FEATURE_TY[lc$FEATURE_TY %in% c("OTHER-LINEAR-FEATURES")] <- "TRAIL"
-lc$FEATURE_TY[lc$FEATURE_TY %in% c("REC-TRAIL")] <- "TRAIL"
-lc$FEATURE_TY[lc$FEATURE_TY %in% c("WOODY-VEGETATON-REMOVAL")] <- "WOODY-VEGETATION-REMOVAL"
-lc$FEATURE_TY <- as.factor(lc$FEATURE_TY)
+d$FEATURE_TY <- as.character(d$FEATURE_TY)
+d$FEATURE_TY[d$FEATURE_TY %in% c("TIMBER-HARVEST-GREEN-AREA", "TIMBER-HARVEST-WHITE-AREA")] <- "HARVEST-AREA"
+d$FEATURE_TY[d$FEATURE_TY %in% c("OTHER-LINEAR-FEATURES")] <- "TRAIL"
+d$FEATURE_TY[d$FEATURE_TY %in% c("REC-TRAIL")] <- "TRAIL"
+d$FEATURE_TY[d$FEATURE_TY %in% c("WOODY-VEGETATON-REMOVAL")] <- "WOODY-VEGETATION-REMOVAL"
+d$FEATURE_TY <- as.factor(d$FEATURE_TY)
 
-lc1 <- lc |> filter(fid_1 %in% as.numeric(levels(as.factor(d$fid_1))[1:100]))
-
-g1 <- g |> filter(fid %in% lc1$fid_1)
-
-ggplot() + geom_sf(data = g1) + geom_sf(data = lc1)
-
-lc2 <- st_drop_geometry(lc1)
+# ggplot() + geom_sf(data = g1) + geom_sf(data = lc1)
 
 # Obtain long summary
 d.long <- make_landcover_long(
-  landcover = lc2,
+  landcover = d,
   col.label = "fid_1",
   col.baseyear = 2022,
   col.hfyear = "YEAR",
@@ -52,7 +40,7 @@ d.long <- make_landcover_long(
   burn.cc = TRUE,
   age.correction = "Age_Correction",
   ver.id = "V7.0"
-)
+); gc()
 
 
 # Obtain wide summary
@@ -67,7 +55,7 @@ d.wide <- make_landcover_wide(
   age.data = "Maltman.Old",
   ver.id = "V7.0",
   rm0 = TRUE
-)
+); gc()
 
 lc.coef.update <- landcover.coef.lookup
 
@@ -94,25 +82,30 @@ landcover.out <- clean_landcover(
   class.in = "ID",
   class.out = "COEF")
 
+# Convert the areas to proportions
+lc.out <- landcover.out/rowSums(landcover.out)
+summary(rowSums(lc.out))
+
+
 library(ABMIexploreR)
 
 # Load the bioclimatic raster into memory
 abmi_load_bioclimatic()
 
-cent <- st_centroid(g1)
+cent <- st_centroid(g)
 
 # Extraction of single species coeffcients for vegetation models
 single.species <- coefficient_extraction(species = "BlackthroatedGreenWarbler", model = "Vegetation")
 str(single.species)
 
-plot_coef(species = "BlackthroatedGreenWarbler", model = "Vegetation")
+# plot_coef(species = "BlackthroatedGreenWarbler", model = "Vegetation")
 
-spatial.locations <- vect(st_coordinates(cent), type = "point")
+spatial.locations <- vect(cent)
 crs(spatial.locations) <- "EPSG:3400"
 
 climate.input <- extract_climate(spatial.grid = spatial.locations, 
                                  cell.id = cent$fid,
-                                 reproject = FALSE)
+                                 reproject = FALSE); gc()
 
 
 ## define species and bootstrap id
@@ -120,12 +113,14 @@ spp <- "BlackthroatedGreenWarbler"
 i <- 0
 
 ## Define the compositional data
-veg.data <- landcover.out
+veg.data <- lc.out
 
 rm <- which(is.na(climate.input$wN))
 
 climate.input.rm <- climate.input[-rm, ]
 veg.data.rm <- veg.data[-which(rownames(veg.data) %in% rownames(climate.input)[rm]), ]
+
+save(climate.input.rm, veg.data.rm, spatial.locations, file = "2_pipeline/store/abmi_prediction_data.Rdata")
 
 ## Make single species prediction
 model.output <- species_predict(species = spp, 
@@ -134,7 +129,25 @@ model.output <- species_predict(species = spp,
                                 modified = FALSE, 
                                 boot = i)
 
-saveRDS(model.output, "2_pipeline/store/btnw_g2_test_predictions.rds")
+saveRDS(model.output, "2_pipeline/store/btnw_osr_predictions_1km_prop.rds")
+
+truncated.pred <- truncate_predict(spp, current = model.output$Vegetation, reference = NULL)
+str(truncated.pred)
+summary(truncated.pred$current)
+
+## Convert densities to abundance
+btnw_abundance <- truncated.pred$current*10^2
+summary(btnw_abundance)
 
 
+# Create raster map from prediction 
+species.pred <- spatial.locations[match(names(btnw_abundance), spatial.locations$fid), ]
+species.pred$Species <- btnw_abundance
+species.pred <- rast(x = species.pred,
+                     type = "xyz")
+# crs(species.pred) <- "EPSG:3400"
 
+plot_species(spat.raster = species.pred,
+             variable = "Species")
+
+writeRaster(species.pred, filename = "2_pipeline/store/spp_pred/btnw_osr.tif")
